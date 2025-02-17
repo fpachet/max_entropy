125af55948c7325d677536f993ecc488c8bbac4e
Pierre Roy <pierre.roy@soundtrap.com>
2025-02-17 16:20:43 +0100

    add indices for Z (normalization)

--- core/preprocess_indices.py
+++ core/preprocess_indices.py
@@ -39,6 +39,7 @@ def compute_contexts(idx_seq, /, *, kmax, left_padding=-1, right_padding=-2):
     assert c.shape == (m, l)
     return c
 
+
 @timeit
 def compute_context_indices(contexts, kmax=0):
     """
@@ -46,15 +47,15 @@ def compute_context_indices(contexts, kmax=0):
 
     The result R is a 3D-array of shape (m, 3, 2•kmax). For a given context µ,
     R[µ] is a (3, 2•kmax) array such that J[R[µ]] is the interaction potential values
-    corresponding to the context µ. If the context is [A,B, C, D, E, F, G] (assuming
+    corresponding to the context µ. If the context is [A, B, C, D, E, F, G] (assuming
     kmax = 3), then D is s_0 (the center of the context) and
-        R[µ][0] = [0, 0, 1, 1, 2, 2]
-        R[µ][1] = [C, D, B, D, A, D]
-        R[µ][2] = [D, E, D, F, D, G]
+        R[µ, 0] = [0, 0, 1, 1, 2, 2]
+        R[µ, 1] = [C, D, B, D, A, D]
+        R[µ, 2] = [D, E, D, F, D, G]
 
     Since J is indexed by K, left index, right index, J[R[µ]] will give the interaction
     potentials:
-        J[0,C,D], J[0, D, E] // the potentials at distance 1 from the center
+        J[0, C, D], J[0, D, E] // the potentials at distance 1 from the center
         J[1, B, D], J[1, D, F] // the potentials at distance 2 from the center
         J[2, A, D], J[2, D, G] // the potentials at distance 3 from the center
 
@@ -72,25 +73,26 @@ def compute_context_indices(contexts, kmax=0):
     m, ctx_length = contexts.shape
     kmax = kmax or (ctx_length - 1) // 2
 
-    indices_in_j = np.tile(np.arange(kmax).repeat(2), m).reshape(m, -1)
+    k_indices = np.tile(np.arange(kmax).repeat(2), m).reshape(m, -1)
 
     left_indices = np.zeros(2 * kmax, dtype=int)
     left_indices[0::2] = np.arange(kmax - 1, -1, -1)
     left_indices[1::2] = kmax
 
     right_indices = np.zeros(2 * kmax, dtype=int)
-    right_indices[1::2] = np.arange(kmax + 1, 2 * kmax+1)
+    right_indices[1::2] = np.arange(kmax + 1, 2 * kmax + 1)
     right_indices[0::2] = kmax
 
     left_indices = contexts[:, left_indices]
     right_indices = contexts[:, right_indices]
 
-    x = np.zeros((m, 3, 2 * kmax), dtype=int)
-    x[:, 0, :] = indices_in_j
-    x[:, 1, :] = left_indices
-    x[:, 2, :] = right_indices
-    return x
+    indices = np.zeros((m, 3, 2 * kmax), dtype=int)
 
+    indices[:, 0, :] = k_indices
+    indices[:, 1, :] = left_indices
+    indices[:, 2, :] = right_indices
+
+    return indices
 
 
 @timeit
@@ -116,21 +118,55 @@ def compute_context_indices_naive(contexts, kmax=0):
     return result
 
 
-def compute_normalization_indices():
-    pass
+@timeit
+def compute_normalization_context_indices(contexts, q: int, kmax: int = 0):
+    """
+    Compute the indices of each context in J with respect to each 0 ≤ sigma < q.
+
+    The result R is a 4D-array of shape (m, q, 3, 2•kmax). For a given context µ, and
+    an element index σ (0 ≤ σ < q), R[µ, σ] is a (3, 2•kmax) array such that J[R[µ, σ]]
+    is the interaction potential values corresponding to the context µ if the
+    context center is σ. If the context is [A, B, C, D, E, F, G] (assuming
+    kmax = 3), then for a given σ:
+        R[µ, σ, 0] = [0, 0, 1, 1, 2, 2]
+        R[µ, σ, 1] = [C, σ, B, σ, A, σ]
+        R[µ, σ, 2] = [σ, E, σ, F, σ, G]
+
+    Since J is indexed by K, left index, right index, J[R[µ]] will give the interaction
+    potentials:
+        J[0, C, σ], J[0, σ, E]
+        J[1, B, σ], J[1, σ, F]
+        J[2, A, σ], J[2, σ, G]
+
+    Therefore J[R[µ, σ]].sum() will give the total interaction potential for the context
+    µ and elements index σ, as computer in the formula TODO reference to formula
+    in the paper TODO reference to paper.
+
+    Args:
+        contexts: the 2D-array of contexts
+        q: the size of the alphabet
+        kmax: the maximum distance from the center of the context
+
+    Returns:
+        a 4D-array fo shape (m, q, 3, 2•kmax)
+    """
+    indices = compute_context_indices(contexts, kmax)
+    normalization_indices = np.tile(
+        indices.reshape(indices.shape[0], 1, *indices.shape[1:]),
+        (1, q, 1, 1))
+    for sigma in range(q):
+        normalization_indices[:, sigma, 1, 1::2] = sigma
+        normalization_indices[:, sigma, 2, 0::2] = sigma
+
+    return normalization_indices
 
 
 if __name__ == '__main__':
-
     mat = np.arange(28).reshape((7, 4))
-    print(mat)
-    x = mat[(np.arange(7), [1, 2, 1, 3, 1, 2, 1])]
-    print(x)
-    y = mat[:, [1, 2, 1, 3, 1, 2 ,1]]
-    print(y)
-
-
-    seq = np.arange(1e6)
-    ctx = compute_contexts(seq, kmax=5)
-    indices = compute_context_indices(ctx)
-
+    g = np.random.default_rng(10)
+    q = 3
+    kmax = 3
+    seq = g.integers(0, q, 10)
+    ctx = compute_contexts(seq, kmax=kmax)
+    j_idx = compute_context_indices(ctx, kmax=kmax)
+    z_idx = compute_normalization_context_indices(ctx, q=q, kmax=kmax)
