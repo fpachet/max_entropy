f5e41953179d6e8ca1c8a685beb2c89900845ffc
Pierre Roy <pierre.roy@soundtrap.com>
2025-03-01 16:56:15 +0100

    in progress

--- maxent_np/max_ent_np.py
+++ maxent_np/max_ent_np.py
@@ -17,44 +17,71 @@ from utils.profiler import timeit
 
 
 class MaxEnt:
+    """
+    A class to represent a Maximum Entropy model.
+
+    Attributes:
+        S: list[int] — 1D-numpy array of shape (M), S[µ] is the index
+            of the symbol at position µ in the training sequence
+
+        M: int — the number of symbols in the training sequence; the typical index used
+            to denote an index of an element in the training sequence is µ
+
+        q: int — the number of symbols in the vocabulary; the typical index used to
+            denote an index of a symbol in the vocabulary is σ; if another symbol is
+            needed, τ is used
+
+        K: int — the context width, i.e., the maximum distance from the center of
+            a context; the typical index used to denote a distance from the center of a
+            context is k
+
+        J: npt.NDArray[float] — 3D-numpy array of shape (K, q, q), element
+            J(k, σ, τ) is the interaction potential between symbols σ and τ at
+            distance k from each other within a context
+
+        h: np.ndarray — 1D-numpy array of shape (q), h[σ] is the bias of symbol σ
+
+        C: NDArrayInt — 2D-numpy array of shape (M, 2•kmax + 1), contexts[µ] is
+            the context centered around the symbol at position µ
+
+        Z: npt.NDArray[float] — 1D-numpy array of shape (M), Z[µ] is the partition
+            function for the context centered around the symbol at position µ
+    """
+
     PADDING = -1
 
-    # 1D-numpy array of shape (M), training_seq[µ] is the index of the symbol at
-    # position µ in the training sequence
-    ix_seq: list[int]
-
-    # The number of symbols in the training sequence
+    S: list[int]
     M: int
-
-    # The number of symbols in the vocabulary
     q: int
-
-    # The context width, i.e., the maximum distance from the center of a context
-    Kmax: int
-
-    # 3D-numpy array of shape (kmax, q, q), J(k, σ, τ) is the interaction potential
-    # between symbols σ and τ at distance k from each other within a context
+    K: int
     J: npt.NDArray[float]
-
-    # 1D-numpy array of shape (q), h[σ] is the bias of symbol σ
     h: np.ndarray
-
-    # 2D-numpy array of shape (M, 2•kmax + 1), contexts[µ] is the context centered
-    # around the symbol at position µ
-    contexts: NDArrayInt
-
-    # The left-padding index, usually -1
-    left_padding: int
-
-    # The right-padding index, usually -2
-    right_padding: int
-
-    # The partition function, this is a 1D-array of shape (M)
+    C: NDArrayInt
     Z: npt.NDArray[float]
 
-    Z_ix: tuple[NDArrayInt, ...]
-    L_ix1: NDArrayInt
-    L_ix: tuple[NDArrayInt, ...]
+    """
+    The indices of the contexts in J, the 3D-array of interaction potentials used
+    to compute the total interaction potential for each context; the shape of L_ix_arr
+    is (M, 3, 2•kmax), the typical usage is Z_ix_arr[µ] to get the indices
+    of the interaction potentials for the context centered around symbol µ: the result
+    is a 3D-array of shape (3, 2•kmax) such that J[L_ix_arr[µ]] contains all the 
+    interaction potentials for the context centered around symbol µ at all distances.
+    """
+    L_ix_arr: NDArrayInt
+
+    # The indices of the contexts in J, the 3D-array of interaction potentials used
+    # to compute the total interaction potential for each context when replacing the
+    # center of the context with a symbol from the vocabulary; the shape of Z_ix_arr
+    # is (M, 3, q, 2•kmax), the typical usage is Z_ix_arr[µ, σ] to get the indices of
+    # the interaction potentials of the context centered around symbol µ with the center
+    # replaced by symbol σ: the result is a 2D-array of shape (3, 2•kmax) such that
+    # J[Z_ix_arr[µ, σ]] contains all the interaction potentials for the context centered
+    # around symbol µ with the center replaced by symbol σ at all distances.
+    partition_ix: NDArrayInt
+
+    J6: tuple[NDArrayInt, ...]
+
+    J6: tuple[NDArrayInt, ...]
 
     # λ in the paper, the lambda regularization parameter
     l: float
@@ -67,10 +94,10 @@ class MaxEnt:
         kmax,
         l=1.0,
     ):
-        self.ix_seq: list[int] = index_training_seq
-        self.M: int = len(self.ix_seq)
+        self.S: list[int] = index_training_seq
+        self.M: int = len(self.S)
         self.q: int = q
-        self.Kmax: int = kmax
+        self.K: int = kmax
         self.l = l
 
         self.Z = np.zeros(self.M, dtype=float)
@@ -82,42 +109,46 @@ class MaxEnt:
         # init J with j_init and an additional row of zeros at the end and an
         # additional column of zeros at the end of each row
 
-        self.J = np.zeros((self.Kmax, self.q + 1, self.q + 1), dtype=float)
+        self.J = np.zeros((self.K, self.q + 1, self.q + 1), dtype=float)
         j_init = np.linspace(0, 1, self.q**2).reshape(self.q, self.q)
         self.J[:, : self.q, : self.q] = j_init
 
-        self.contexts = compute_contexts(
+        self.C = compute_contexts(
             index_training_seq,
-            kmax=self.Kmax,
+            kmax=self.K,
             padding=MaxEnt.PADDING,
         )
+
+        self.L_ix_arr = compute_context_indices(self.C, self.K)
+        _indices = compute_context_indices(self.C, self.K)
+        _indices = np.swapaxes(_indices, 0, 1)
+        _indices = np.reshape(_indices, (3, -1))
+        self.J5 = tuple(row for row in _indices)
 
         # get the indices in J of the contexts prepared in such a way that J[self.Z_ix]
         # returns the potential values for all contexts in a single 1D-array
         # see compute_z() for more details on how this is used
-        self.Z_ix1 = compute_partition_context_indices(
-            self.contexts, q=self.q, kmax=self.Kmax
+        self.partition_ix = compute_partition_context_indices(
+            self.C, q=self.q, kmax=self.K
         )
-        _indices = compute_partition_context_indices(
-            self.contexts, q=self.q, kmax=self.Kmax
-        )
-        _indices = np.swapaxes(_indices, 1, 2)
-        _indices = np.reshape(_indices, (self.M, 3, -1))
-        _indices = np.swapaxes(_indices, 0, 1)
-        _indices = np.reshape(_indices, (3, -1))
-        self.Z_ix = tuple(row for row in _indices)
-
-        self.L_ix1 = compute_context_indices(self.contexts, self.Kmax)
-        _indices = compute_context_indices(self.contexts, self.Kmax)
-        _indices = np.swapaxes(_indices, 0, 1)
-        _indices = np.reshape(_indices, (3, -1))
-        self.L_ix = tuple(row for row in _indices)
+        _indices = self.partition_ix  # (M, q, 3, 2•K)
+        _indices = np.swapaxes(_indices, 1, 2)  # (M, 3, q, 2•K)
+        _indices = np.reshape(_indices, (self.M, 3, -1))  # (M, 3, 2•K•q)
+        _indices = np.swapaxes(_indices, 0, 1)  # (3, M, 2•K•q)
+        _indices = np.reshape(_indices, (3, -1))  # (3, M•2•K•q)
+        self.J6 = tuple(row for row in _indices)
+        _indices = self.partition_ix  # (M, q, 3, 2•K)
+        _indices = np.swapaxes(_indices, 0, 1)  # (q, M, 3, 2•K)
+        _indices = np.swapaxes(_indices, 1, 2)  # (q, 3, M, 2•K)
+        _indices = np.swapaxes(_indices, 0, 1)  # (3, q, M, 2•K)
+        _indices = np.reshape(_indices, (3, -1))  # (3, q•M•2•K)
+        self.J7 = tuple(row for row in _indices)
 
         # get the partition context indices and converts the 4D-array of shape (M, q, 3, 2•kmax) into a list
         # of length M, each element is a list of length q whose elements are tuple of three 2•kmax numpy vectors
         # This is to make J[self.partition_context_indices[mu, sigma]] return the result using numpy matrix indexing
         _partition_context_indices = compute_partition_context_indices(
-            self.contexts, q=self.q, kmax=self.Kmax
+            self.C, q=self.q, kmax=self.K
         )
         self.partition_context_indices = []
         for row_mu in _partition_context_indices:
@@ -135,14 +166,15 @@ class MaxEnt:
         Compute the partition function Z for each context µ in the training sequence.
 
         Does it without using for loops by using numpy matrix indexing. See how
-        self.Z_ix is laid out in the __init__ method for more details.
+        self.J6 is laid out in the __init__ method for more details.
 
-        Formula (5) in the referenced paper.
+        Formula (6) in the referenced paper.
         """
 
         # all_j is essentially J[self.Z_ix] for all mu < self.M and all sigma < self.q
         # first, all_j is a 1D-array of shape (M * q * 2•kmax)
-        all_j = self.J[self.Z_ix]
+        all_j = self.J[self.J6]
+
         # then it is reshaped into a 3D-array of shape (M, q, 2•kmax)
         # so all_j[mu, sigma] is the array with all interaction potentials
         all_j = np.reshape(all_j, (self.M, self.q, -1))
@@ -175,8 +207,8 @@ class MaxEnt:
         Returns:
             a float (the NLL)
         """
-        sum_h = self.h[self.ix_seq].sum()
-        sum_j = self.J[self.L_ix].sum()
+        sum_h = self.h[self.S].sum()
+        sum_j = self.J[self.J5].sum()
         log_z = np.log(self.Z).sum()
         norm1_j = np.sum(np.abs(self.J))
         return (-(sum_h + sum_j - log_z) + self.l * norm1_j) / self.M
@@ -192,10 +224,28 @@ class MaxEnt:
         for r in range(self.q):
             dg_dh[r] = 0
             for mu in range(self.M):
-                if self.ix_seq[mu] == r:
+                if self.S[mu] == r:
                     dg_dh[r] += 1
                 dg_dh[r] -= (1 / self.Z[mu]) * np.exp(
-                    self.h[r] + self.J[tuple(self.Z_ix1[mu, r])].sum()
+                    self.h[r] + self.J[tuple(self.partition_ix[mu, r])].sum()
+                )
+        return -dg_dh / self.M
+
+    @timeit
+    def grad_loc_field_slow(self):
+        """
+        Formula (7) in the referenced paper.
+        Returns:
+            a 1D-numpy array of shape (q)
+        """
+        dg_dh = np.zeros(self.q)
+        for r in range(self.q):
+            dg_dh[r] = 0
+            for mu in range(self.M):
+                if self.S[mu] == r:
+                    dg_dh[r] += 1
+                dg_dh[r] -= (1 / self.Z[mu]) * np.exp(
+                    self.h[r] + self.J[tuple(self.partition_ix[mu, r])].sum()
                 )
         return -dg_dh / self.M
 
@@ -206,8 +256,8 @@ class MaxEnt:
         Returns:
             a 1D-numpy array of shape (q)
         """
-        dg_dJ = np.zeros((self.Kmax, self.q, self.q))
-        for k in range(self.Kmax):
+        dg_dJ = np.zeros((self.K, self.q, self.q))
+        for k in range(self.K):
             for r in range(self.q):
                 for r2 in range(self.q):
                     ...
